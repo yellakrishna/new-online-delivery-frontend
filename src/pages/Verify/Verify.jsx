7c6c82ae64411881dbc9e24b0a3635c348297e12
yellakrishna <yellakrishna7675@gmail.com>
2025-08-05 16:06:44 +0530

    some change navigate

--- src/pages/Verify/Verify.jsx
+++ src/pages/Verify/Verify.jsx
@@ -15,7 +15,7 @@ const Verify = () => {
   const verifyPayment = async () => {
     const response = await axios.post(url + "/api/order/verify", { success, orderId });
     if (response.data.success) {
-      navigate("/myorders");
+      navigate("/order-success");
     }
     else {
       navigate("/")
